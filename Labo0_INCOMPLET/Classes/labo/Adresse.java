7c02312d81d804a9173760cdca58e2b8ae938864
medali.bettaieb <medali.bettaieb@esprit.tn>
2014-05-29 18:56:26 +0200

    first PBL labo
    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

---
+++ Labo0_INCOMPLET/Classes/labo/Adresse.java
@@ -0,0 +1,21 @@
+
+package labo;
+
+public class Adresse {
+
+    public int codePostal;
+    public String gouvernorat;
+    public String ville;
+    
+    public Adresse() {
+    }
+    
+    public Adresse(int codePostal, String gouvernorat, String ville) {
+        this.codePostal = codePostal;
+        this.gouvernorat = gouvernorat;
+        this.ville = ville;
+    }
+
+   
+    
+}
---
+++ Labo0_INCOMPLET/Classes/labo/Test.java
@@ -0,0 +1,46 @@
+package labo;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        
+//        Travail à faire : (voir support du cours)
+//1-	Installer l’environnement  de développement Netbeans et importer le projet.
+//2-	Créer une adresse dans la classe Test en utilisant le 1er constructeur (voir les commentaires).
+Adresse adr;
+adr=new Adresse();
+        //3-	Affecter des valeurs à l’objet adresse créée.
+adr.codePostal=2000;
+adr.gouvernorat="Tunis";
+adr.ville="Tunis";
+
+//4-	Afficher l’objet adresse crée.
+
+//5-	Modifier le code postal de cette adresse.
+//6-	Afficher l’objet adresse modifié.
+//7-	Créer une adresse dans la classe Test en utilisant le 2ème constructeur  (voir les commentaires).
+//8-	Modifier la ville de cette adresse.
+//9-	Afficher l’objet adresse modifié.
+//10-	Ecrivez la classe Chercheur en ajoutant ses attributs.
+//11-	Créer un Chercheur dans la classe Test en utilisant le même type de constructeur dans la classe Adresse C1. 
+//12-	Affecter des valeurs à ce chercheur.
+//13-	Afficher toutes les informations relatives à ce chercheur.
+//14-	Déclarer dans la classe Chercheur un deuxième constructeur paramétré.
+//15-	Afficher toutes les informations relatives à ce chercheur.
+//NB : Vous pouvez utiliser la méthode toString() .
+//16-	Afficher le nombre de chercheurs créés.
+//17-	 Ajouter dans la classe Chercheur la méthode comparer() qui permet de tester l’égalité entre deux chercheurs : public void comparer (Chercheur ch){…}
+
+//18-	
+//19-	Modifier le numéro de l’ordinateur de l’objet  chercheur1
+//20-	Créer un objet chercheur3 sans paramètres
+//21-	Ajouter un nom, un poste et un numéro de l’ordinateur à l’objet chercheur3
+//22-	Créer deux Bureaux bureau1 et bureau2, chaque bureau contient 3 chercheurs
+//23-	Créer un Laboratoire laboratoire1 qui contient deux bureaux.
+//24-	Afficher les caractéristiques de l’objet laboratoire1.
+
+       
+    }
+
+}
